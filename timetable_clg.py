4392439ef71b02f1710dffcfdaa232c793b90043
poomiARV <arvpoomighaa@gmail.com>
2024-05-31 12:00:44 +0530

    pushtest

--- timetable_clg.py
+++ timetable_clg.py
@@ -5,6 +5,7 @@ name="Mr.PKG"
 print("Initial edit by Justin")
 print(f"Then by :{name}")
 print("push command testing")
-print("push command test 2")
+print("push command test 2") 
 print("push command test 4")
 print("push command test 5")
+print("push command test 6")
